80fbc50eee2e6d604efc26023c50551725197428
darkside550 <imran.saad50@gmail.com>
2021-01-26 11:18:07 +0530

    initial commit

---
+++ src/CurrencyRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const CurrencyRow = () => {
+    return (
+        <div>
+            <input type="number"/>
+            <select name="" id="">
+                <option value="Hi">Hi</option>
+            </select>
+        </div>
+    )
+}
